8389f389500864a4e1eea0b1138a7f9e0d8381e2
Ye Joo Park <subwaymatch@gmail.com>
2020-08-18 06:56:33 -0500

    Use AnimatePresnece for exit animation

--- pizzajoint/src/components/Order.js
+++ pizzajoint/src/components/Order.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const containerVariants = {
   hidden: {
@@ -44,6 +44,12 @@ const toppingItemVariants = {
 };
 
 const Order = ({ pizza }) => {
+  const [showTitle, setShowTitle] = useState(true);
+
+  setTimeout(() => {
+    setShowTitle(false);
+  }, 4000);
+
   return (
     <motion.div
       className="container order"
@@ -51,7 +57,11 @@ const Order = ({ pizza }) => {
       initial="hidden"
       animate="visible"
     >
-      <h2>Thank you for your order :)</h2>
+      <AnimatePresence>
+        {showTitle && (
+          <motion.h2 exit={{ y: -1000 }}>Thank you for your order :)</motion.h2>
+        )}
+      </AnimatePresence>
       <motion.p variants={childVariants}>
         You ordered a {pizza.base} pizza with:
       </motion.p>
